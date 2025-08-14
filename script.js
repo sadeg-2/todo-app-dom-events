8b83b3943f81b61bc71aa258c65fb6335cac88b7
sadeg ashour <sadeg.magde@outlook.sa>
2025-08-14 08:20:18 +0300

    update some style and prepare js with dummy test data and usefull function

--- script.js
+++ script.js
@@ -1,2 +1,81 @@
-const element = document.querySelector("h1");
-console.log(element.textContent);
+const tasks = [
+  {
+    text: "Home Work 1",
+    completed: true,
+  },
+  {
+    text: "Home Work 2",
+    completed: false,
+  },
+  {
+    text: "Home Work 3",
+    completed: false,
+  },
+  {
+    text: "Home Work 4",
+    completed: true,
+  },
+];
+
+// to show notify if the add operation successfull or not
+function notify(message) {
+  const node = document.getElementById("alert");
+  node.textContent = message;
+  node.style.display = "block";
+  setTimeout(() => {
+    node.style.display = "none";
+  }, 4000); // show every second
+}
+
+// to creation the element based object
+function creatTaskElement(element) {
+  const task = document.createElement("li");
+  task.classList.add("task");
+  if (element.completed) task.classList.add("complete");
+
+  const content = document.createElement("p");
+  content.textContent = element.text;
+  task.appendChild(content);
+
+  const checkBox = document.createElement("input");
+  checkBox.type = "checkbox";
+  checkBox.checked = element.completed;
+  task.appendChild(checkBox);
+
+  const actionBtn = document.createElement("input");
+  actionBtn.type = "button";
+  actionBtn.value = "Delete";
+  actionBtn.classList.add("remove");
+  task.appendChild(actionBtn);
+
+  return task;
+}
+
+const listTasks = document.querySelector(".list-task");
+
+// here to simulate dummy test
+tasks.forEach(function (element) {
+  const task = creatTaskElement(element);
+  listTasks.appendChild(task);
+});
+
+const addTask = document.getElementById("addTask");
+const inputTask = document.getElementById("inTask");
+
+// here buisness logic to add button action
+addTask.addEventListener("click", function (event) {
+  const taskText = inputTask.value.trim();
+  if (taskText == null || taskText == "") {
+    alert("you should enter task first");
+  } else {
+    const element = { text: taskText, completed: false };
+    tasks[tasks.length] = element;
+    console.log(tasks);
+    const task = creatTaskElement(element);
+    listTasks.appendChild(task);
+    inputTask.value = "";
+    notify(`Task name : ${element.text} was added successfully`);
+  }
+});
+
+
